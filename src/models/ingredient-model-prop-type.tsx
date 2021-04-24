cc1a532a24bffc8a57b17fa9867f2cb82868689b
Roman Volodin <32717824+RomanAVolodin@users.noreply.github.com>
2021-04-24 22:24:13 +0300

    prop types for ingredient fixed

--- src/models/ingredient-model-prop-type.tsx
+++ src/models/ingredient-model-prop-type.tsx
@@ -2,10 +2,18 @@ import PropTypes from 'prop-types';
 
 
 const ingredientPropType = PropTypes.shape({
-    id: PropTypes.number.isRequired,
-    user: PropTypes.string.isRequired,
-    replyTo: PropTypes.number,
-    text: PropTypes.string.isRequired
+    _id: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    type: PropTypes.string.isRequired,
+    proteins: PropTypes.number.isRequired,
+    fat: PropTypes.number.isRequired,
+    carbohydrates: PropTypes.number.isRequired,
+    calories: PropTypes.number.isRequired,
+    price: PropTypes.number.isRequired,
+    image: PropTypes.string.isRequired,
+    image_mobile: PropTypes.string.isRequired,
+    image_large: PropTypes.string.isRequired,
+    __v: PropTypes.number.isRequired
 })
 
 export default ingredientPropType;
